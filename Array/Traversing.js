695789786a9efa67cee9ca2ad45fbd3ba4ef27c1
Akashpvt <155839828+Akashpvt@users.noreply.github.com>
2024-07-14 04:10:07 +0000

    Traversing

---
+++ Array/Traversing.js
@@ -0,0 +1,7 @@
+let data = [9, 34, 5, 45, 73, 65, 11, 8 ];
+//for (let i = 0; i <data.length; i++){
+    //console.log(`Array ${i} is ${data [i]}`);
+    
+//}
+let x = 6;
+console.log(`${data[x]}`);
